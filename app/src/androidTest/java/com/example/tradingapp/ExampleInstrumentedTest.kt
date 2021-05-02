d2bb8e9c2cfbb21138704b091ba0e0c79aaf057b
seejinming <113227937+seejinming@users.noreply.github.com>
2021-05-02 08:43:58 +0000

    Update annotation_tools.md

--- app/src/androidTest/java/com/example/tradingapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/tradingapp/ExampleInstrumentedTest.kt
@@ -11,4 +11,7 @@ import org.junit.Assert.*
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * See [testing docum
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleIns
