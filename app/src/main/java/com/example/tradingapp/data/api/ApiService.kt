f5476a60137b0a2cd4f938f2ef0d85904cde6f5d
seejinming <113227937+seejinming@users.noreply.github.com>
2021-06-27 14:01:15 +0000

    video/udlfb: fix line counting in fb_write

--- app/src/main/java/com/example/tradingapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/tradingapp/data/api/ApiService.kt
@@ -2,4 +2,11 @@ package com.example.tradingapp.data.api
 
 import com.example.tradingapp.model.ProducutListResponce
 import okhttp3.RequestBody
-import retrofit2.Respons
+import retrofit2.Response
+import retrofit2.http.*
+
+interface ApiService {
+
+
+    @GET("ticket/count")
+    susp
