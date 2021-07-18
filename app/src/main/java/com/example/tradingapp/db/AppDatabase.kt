f51956c644c3769f85f6e80e51aa33b453837702
seejinming <113227937+seejinming@users.noreply.github.com>
2021-07-18 17:39:46 +0000

    Fix review issues

--- app/src/main/java/com/example/tradingapp/db/AppDatabase.kt
+++ app/src/main/java/com/example/tradingapp/db/AppDatabase.kt
@@ -3,4 +3,7 @@ package com.example.tradingapp.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.tradingapp.db.UserDetailsDao
-import com.example.tradingapp.model.Use
+import com.example.tradingapp.model.UserDetails
+
+@Database(entities = [UserDetails::class], version = 1)
+abstract class AppDatabase:R
