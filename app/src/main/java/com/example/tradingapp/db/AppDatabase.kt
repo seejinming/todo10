f76019945f6f1b152a2eb75eef486277d663c7c8
seejinming <113227937+seejinming@users.noreply.github.com>
2021-07-18 11:08:09 +0000

    Bump version to 0.0.5

--- app/src/main/java/com/example/tradingapp/db/AppDatabase.kt
+++ app/src/main/java/com/example/tradingapp/db/AppDatabase.kt
@@ -2,4 +2,5 @@ package com.example.tradingapp.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example
+import com.example.tradingapp.db.UserDetailsDao
+import com.example.tradingapp.model.Use
