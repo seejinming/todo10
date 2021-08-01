f04bb6fd701d42df416cdf4bb70f03fa97d5285b
seejinming <113227937+seejinming@users.noreply.github.com>
2021-08-01 18:13:37 +0000

    Regen

--- app/src/main/java/com/example/tradingapp/model/ResponseResource.kt
+++ app/src/main/java/com/example/tradingapp/model/ResponseResource.kt
@@ -1,3 +1,6 @@
 package com.example.tradingapp.model
 
-data class ResponseResource<out T>(val 
+data class ResponseResource<out T>(val status: Status, val data: T?, val message: String?) {
+
+    enum class Status {
+        SUCCES
