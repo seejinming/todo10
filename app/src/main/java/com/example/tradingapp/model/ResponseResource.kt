b87a60210beb5556cd9a1e2e6a079fb6bb24240a
seejinming <113227937+seejinming@users.noreply.github.com>
2021-08-08 07:28:57 +0000

    better icon

--- app/src/main/java/com/example/tradingapp/model/ResponseResource.kt
+++ app/src/main/java/com/example/tradingapp/model/ResponseResource.kt
@@ -3,4 +3,10 @@ package com.example.tradingapp.model
 data class ResponseResource<out T>(val status: Status, val data: T?, val message: String?) {
 
     enum class Status {
-        SUCCES
+        SUCCESS,
+        ERROR,
+        LOADING,
+        FAILURE,
+        NOINTERNET,
+        DATAINSETED
+ 
