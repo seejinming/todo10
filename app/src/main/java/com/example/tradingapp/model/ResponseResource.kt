5da2b7ddf84486416ee3f9909eacf5a364fe32fc
seejinming <113227937+seejinming@users.noreply.github.com>
2021-08-24 12:11:00 +0000

    correct syntax error

--- app/src/main/java/com/example/tradingapp/model/ResponseResource.kt
+++ app/src/main/java/com/example/tradingapp/model/ResponseResource.kt
@@ -33,4 +33,5 @@ data class ResponseResource<out T>(val status: Status, val data: T?, val message
             return ResponseResource(Status.FAILURE, data, message)
         }
 
-        fun <T> dataInsert(message: String ): R
+        fun <T> dataInsert(message: String ): ResponseResource<T> {
+            return ResponseResource(Status.DATAINSETED, null, m
