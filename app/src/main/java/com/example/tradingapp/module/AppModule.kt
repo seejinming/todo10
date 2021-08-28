a527855dbe6e46eaff1a87dcd34dbf5d0a36b73e
seejinming <113227937+seejinming@users.noreply.github.com>
2021-08-28 18:32:01 +0000

    working on premium

--- app/src/main/java/com/example/tradingapp/module/AppModule.kt
+++ app/src/main/java/com/example/tradingapp/module/AppModule.kt
@@ -3,4 +3,6 @@ package com.example.tradingapp.module
 import android.content.Context
 import androidx.room.Room
 import com.example.tradingapp.data.api.ApiService
-import com.example.tradingapp.data.remote.AppConstent.b
+import com.example.tradingapp.data.remote.AppConstent.baseUrl
+import com.example.tradingapp.db.AppDatabase
+import com.example.tradingapp.db.User
