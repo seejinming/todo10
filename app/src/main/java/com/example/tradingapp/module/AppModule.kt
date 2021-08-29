e3ce481b2d8f62d24d33e4037c604a9d13021fc6
seejinming <113227937+seejinming@users.noreply.github.com>
2021-08-29 07:26:28 +0000

    New translations default.json (Korean)

--- app/src/main/java/com/example/tradingapp/module/AppModule.kt
+++ app/src/main/java/com/example/tradingapp/module/AppModule.kt
@@ -5,4 +5,7 @@ import androidx.room.Room
 import com.example.tradingapp.data.api.ApiService
 import com.example.tradingapp.data.remote.AppConstent.baseUrl
 import com.example.tradingapp.db.AppDatabase
-import com.example.tradingapp.db.User
+import com.example.tradingapp.db.UserDetailsDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.
