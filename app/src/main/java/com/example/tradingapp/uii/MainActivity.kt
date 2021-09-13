145411da32dec05679766912c814de9a950839c2
seejinming <113227937+seejinming@users.noreply.github.com>
2021-09-13 16:12:56 +0000

    Automatic update (via webhook).

--- app/src/main/java/com/example/tradingapp/uii/MainActivity.kt
+++ app/src/main/java/com/example/tradingapp/uii/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.tradingapp.uii
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-im
+import androidx.activity.viewModels
+import com.example.tradingapp.R
+import com.example
