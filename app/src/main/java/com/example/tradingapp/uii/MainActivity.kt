2de8dcefa54724f375e96a710a7d18a29912b19f
seejinming <113227937+seejinming@users.noreply.github.com>
2021-09-14 13:58:02 +0000

    Revert "Fix tests"

--- app/src/main/java/com/example/tradingapp/uii/MainActivity.kt
+++ app/src/main/java/com/example/tradingapp/uii/MainActivity.kt
@@ -4,4 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.tradingapp.R
-import com.example
+import com.example.tradingapp.model.ResponseResource
+import com.example.tradingapp.model.UserDetails
+i
