2ef159bf2d914bb74a525880bd56c5dfad6f3bdc
seejinming <113227937+seejinming@users.noreply.github.com>
2021-10-07 12:18:59 +0000

    Update dev

--- app/src/main/java/com/example/tradingapp/uii/MainViewModel.kt
+++ app/src/main/java/com/example/tradingapp/uii/MainViewModel.kt
@@ -2,4 +2,6 @@ package com.example.tradingapp.uii
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Vi
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.trad
